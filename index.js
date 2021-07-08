92d3c9a663fb39fc175ed1d763321a7237e711fc
Daniel Kaniu <danniu254@gmail.com>
2021-07-08 18:48:40 +0300

    Css + finishing touches

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
 //
+//Greetings function.
+function display_greetings() {
+//    alert("test");
+    //
+    //Date
+    var today = new Date();
+    //
+    //Extract the current hour from the date.
+    var hourNow = today.getHours();
+    //
+    //Store the greeting here.
+    var greeting;
+    //
+    //Condition.
+    if (hourNow > 15) {
+        //
+        //Evening.
+        greeting = "Good Evening";
+    }
+    else if (hourNow > 12) {
+        //Afternoon.
+        greeting = "Good Afternoon";
+    }
+    else if (hourNow > 0) {
+        //
+        //Morning.
+        greeting = "Good Morning";
+    }
+    else {
+        greeting = "Welcome";
+    }
+    return greeting;
+}
 //
-function status(){
+var i = 0;
+//
+//This is the function that returns a greeting. We have it here to pass it to the function
+//that sets the speed at which the greeting is displayed.
+var txt = display_greetings();
+//
+//The speed at which to display the greeting message.
+var speed = 250;
+//
+//This function gets the greeting message and sets the speed at which the message is displayed.
+function typeWriter() {
     //
-    //
-    const tds = document.querySelectorAll(".qty");
-    //
-    //
-    for(var i = 0; i<tds.length; i++){
+    //Get the length of the greeting message.
+    if (i < txt.length) {
         //
+        //Display the greeting message.
+        document.getElementById("greetings").innerHTML += txt.charAt(i);
+        i++;
         //
-        if(tds[i].textContent < 500){
-            //
-            //
-            
-        }
+        //Set the speed at which the greeting is displayed.
+        setTimeout(typeWriter, speed);
     }
 }
